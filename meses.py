c9943467013d44794590a8c80c180ff22c771ab2
ByDiego20 <55709087+ByDiego20@users.noreply.github.com>
2019-10-24 09:29:44 +0200

    Add files via upload

---
+++ meses.py
@@ -0,0 +1,32 @@
+
+
+def meses():
+    i=input(str("inserta un numero del 1 al 12: "))
+                
+    if i is 1:
+        print("Enero")
+    if i is 2:
+        print("Febrero")
+    if i is 3:
+        print("Marzo")
+    if i is 4:
+        print("Abril")
+    if i is 5:
+        print("Mayo")
+    if i is 6:
+        print("Junio")
+    if i is 7:
+        print("Julio")
+    if i is 8:
+        print("Agosto")
+    if i is 9:
+        print("Septiembre")
+    if i is 10:
+        print("Octubre")
+    if i is 11:
+        print("Noviembre")
+    if i is 12:
+        print("Diciembre")
+
+
+meses()
