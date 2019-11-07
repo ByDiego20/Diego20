a0f9a27c70484e7cfaf55e71597bbf2357260b06
ByDiego20 <55709087+ByDiego20@users.noreply.github.com>
2019-11-07 09:06:00 +0100

    Add files via upload

---
+++ operaciones elementales.py
@@ -0,0 +1,25 @@
+def operaciones_elementales ():
+
+    i=float(input("Introduce un numero: "))
+    a=float(input("Introduzca otro numero: "))
+    letra=raw_input("Introduce una palabra: ")
+
+    
+    print ("S=sumar")
+    print ("R=restar")
+    print ("M=multiplicar")
+    print ("D=dividir")
+    print (" ")
+
+    if letra == "S":
+        print(a+ b)
+    elif letra == "R":
+        print(a- b)
+    elif letra == "M":
+        print(a* b )
+    elif letra == "D":
+        print(a/ b)
+
+operaciones_elementales()
+
+    
